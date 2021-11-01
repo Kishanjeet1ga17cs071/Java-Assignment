c55822b629e0db7f40bca9c482dfc3d16a6bb9e9
Kishanjeet1ga17cs071 <92724262+Kishanjeet1ga17cs071@users.noreply.github.com>
2021-11-01 16:10:27 +0530

    Create Employee.java

---
+++ Test/Employee.java
@@ -0,0 +1,49 @@
+package com.Assesment;
+
+public class Employee {
+	private int employeeId ;
+	private String employeeName ;
+	private double basicSalary ;
+	private double specialAllowances;
+	private double hra ;
+	private double transportAllowances ;
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getBasicSalary() {
+		return basicSalary;
+	}
+	public void setBasicSalary(double basicSalary) {
+		this.basicSalary = basicSalary;
+	}
+	public double getSpecialAllowances() {
+		return specialAllowances;
+	}
+	public void setSpecialAllowances(double specialAllowances) {
+		this.specialAllowances = specialAllowances;
+	}
+	public double getHra() {
+		return hra;
+	}
+	public void setHra(double hra) {
+		this.hra = hra;
+	}
+	public double getTransportAllowances() {
+		return transportAllowances;
+	}
+	public void setTransportAllowances(double transportAllowances) {
+		this.transportAllowances = transportAllowances;
+	}
+	
+	
+
+}
